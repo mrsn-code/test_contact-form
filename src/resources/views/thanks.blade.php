eb4100aeb8663607e8efd1a23ee6154129f46894
mrsn-code <maltifunctionist@gmail.com>
2026-01-12 23:40:35 +0900

    finished

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 </head>
 <body>
-  <h1>お問い合わせありがとうございました</h1>
+  <div>
+    <h1>お問い合わせありがとうございました</h1>
+  </div>
   <form action="/" method="get">
     <button type="submit">home</button>
   </form>
